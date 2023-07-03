59c22553c186d48db8bfa49a0177a430dfe006f7
Nachiket <nachiketmali756@gmail.com>
2023-07-03 16:03:54 +0530

    binded login w counter

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import Login from "./components/login/Login";
 import { useState } from "react";
 import Counter from "./components/counter/Counter";
 
-const DUMMY_INFO = [
-  {
-    username: "abcdef",
-    password: "12345",
-  },
-  { username: "ghijkl", password: "67890" },
-];
+const DUMMY_INFO = {
+  username: "abcdef",
+  password: "12345",
+};
 
 function App() {
   const [isValid, setIsValid] = useState(false);
+  const [error, setError] = useState(false);
 
   const validHandler = (info) => {
-    setIsValid(true);
+    if (
+      info.username === DUMMY_INFO.username &&
+      info.password === DUMMY_INFO.password
+    ) {
+      setIsValid(true);
+      setError(false);
+    } else {
+      setError(true);
+    }
   };
 
   return (
     <div>
-      {isValid ? <Counter></Counter> : <Login onValid={validHandler}></Login>}
+      {!isValid && <Login onValid={validHandler}></Login>}
+      {error && <h3>Enter correct username and password.</h3>}
+      {isValid && <Counter></Counter>}
     </div>
   );
 }
